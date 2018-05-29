f485c8f3409f53dc749f68cb54ccfee0c648ca71
Hugh Wang <hghwng@gmail.com>
2018-05-29 11:11:24 +0800

    main: refactor with closure

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@ extern crate undrift_gps;
 #[macro_use]
 extern crate failure;
 
+use quick_xml::events::{BytesStart, Event};
+use quick_xml::{Reader, Writer};
 use std::convert::From;
 use std::f64::NAN;
+use std::io::{BufRead, Write};
 
 #[derive(Debug, Fail)]
 enum Error {
@@ -14,104 +17,57 @@ enum Error {
     ValueError(usize),
 }
 
+type Result<T> = std::result::Result<T, Error>;
+
 impl From<quick_xml::Error> for Error {
     fn from(e: quick_xml::Error) -> Self {
         Error::XMLError(e)
     }
 }
 
-fn convert_gpx<R, W>(
-    ref mut reader: quick_xml::Reader<R>,
-    ref mut writer: quick_xml::Writer<W>,
-    convert_fn: fn(f64, f64) -> (f64, f64),
-) -> std::result::Result<(), Error>
+fn xml_transform<R: BufRead, W: Write, F>(
+    mut reader: Reader<R>,
+    mut writer: Writer<W>,
+    levels: &[&[u8]],
+    mut transformer: F,
+) -> Result<()>
 where
-    R: std::io::BufRead,
-    W: std::io::Write,
+    F: for<'a> FnMut(BytesStart<'a>, &Reader<R>) -> Result<BytesStart<'a>>,
 {
-    use quick_xml::events::BytesStart;
-    use quick_xml::events::Event;
-    const TRKPT_NAME: &[u8] = b"trkpt";
-    const LAT_NAME: &[u8] = b"lat";
-    const LON_NAME: &[u8] = b"lon";
-    const LEVELS: [&[u8]; 4] = [b"gpx", b"trk", b"trkseg", b"trkpt"];
+    let mut matched = 0;
+    let mut current = 0;
 
-    let mut level_matched = 0;
-    let mut level_current = 0;
     let mut buf = Vec::new();
     loop {
         match reader.read_event(&mut buf) {
-            Ok(Event::Start(elem)) => {
-                // Maintain current level
-                {
-                    if level_matched == level_current && level_matched < LEVELS.len()
-                        && LEVELS[level_matched] == elem.name()
-                    {
-                        level_matched += 1;
-                    }
-                    level_current += 1;
-                    if level_current != level_matched || level_matched != LEVELS.len() {
-                        writer.write_event(Event::Start(elem))?;
-                        continue;
-                    }
-                }
-
-                // Convert Element
-                {
-                    let mut new_elem = BytesStart::owned(TRKPT_NAME.to_owned(), TRKPT_NAME.len());
-                    let mut lat: f64 = NAN;
-                    let mut lon: f64 = NAN;
-
-                    for attr in elem.attributes() {
-                        let attr = try!(attr);
-                        if attr.key != LAT_NAME && attr.key != LON_NAME {
-                            new_elem.push_attribute(attr);
-                            continue;
-                        }
-
-                        let value = attr.unescape_and_decode_value(reader)?;
-                        if let Ok(v) = value.parse::<f64>() {
-                            match attr.key {
-                                b"lat" => {
-                                    lat = v;
-                                }
-                                b"lon" => {
-                                    lon = v;
-                                }
-                                _ => {
-                                    panic!("shouldn't reach here");
-                                }
-                            }
-                        } else {
-                            return Err(Error::ValueError(reader.buffer_position()));
-                        }
-                    }
-
-                    if lat.is_nan() || lon.is_nan() {
-                        return Err(Error::ValueError(reader.buffer_position()));
-                    }
-
-                    let (lat, lon) = convert_fn(lat, lon);
-                    new_elem.push_attribute((LAT_NAME, lat.to_string().as_bytes()));
-                    new_elem.push_attribute((LON_NAME, lon.to_string().as_bytes()));
-                    writer.write_event(Event::Start(new_elem))?;
-                }
-            }
-            Ok(Event::End(elem)) => {
-                if level_matched == level_current {
-                    level_matched -= 1;
-                }
-                level_current -= 1;
-                writer.write_event(Event::End(elem))?;
-            }
             Ok(Event::Eof) => {
                 break;
             }
-            Ok(ref evt) => {
-                writer.write_event(evt)?;
+            Ok(Event::Start(elem)) => {
+                if matched == current && matched < levels.len() && levels[matched] == elem.name() {
+                    matched += 1;
+                }
+                current += 1;
+
+                let elem = if matched == current && matched == levels.len() {
+                    transformer(elem, &reader)?
+                } else {
+                    elem
+                };
+                writer.write_event(Event::Start(elem))?;
             }
-            Err(evt) => {
-                return Err(evt.into());
+            Ok(Event::End(elem)) => {
+                if matched == current {
+                    matched -= 1;
+                }
+                current -= 1;
+                writer.write_event(Event::End(elem))?;
+            }
+            Ok(event) => {
+                writer.write_event(event)?;
+            }
+            Err(err) => {
+                return Err(err.into());
             }
         }
         buf.clear();
@@ -120,9 +76,63 @@ where
     Ok(())
 }
 
-fn main() {
+fn gpx_transform<R: BufRead, W: Write>(
+    reader: quick_xml::Reader<R>,
+    writer: quick_xml::Writer<W>,
+    convert_fn: fn(f64, f64) -> (f64, f64),
+) -> Result<()> {
+    const LAT_NAME: &[u8] = b"lat";
+    const LON_NAME: &[u8] = b"lon";
+    const LEVELS: [&[u8]; 4] = [b"gpx", b"trk", b"trkseg", b"trkpt"];
+
+    xml_transform(reader, writer, &LEVELS, |elem, reader| {
+        let mut new_elem = BytesStart::owned(elem.name().to_vec(), elem.name().len());
+        let mut lat: f64 = NAN;
+        let mut lon: f64 = NAN;
+
+        for attr in elem.attributes() {
+            let attr = try!(attr);
+            if attr.key != LAT_NAME && attr.key != LON_NAME {
+                new_elem.push_attribute(attr);
+                continue;
+            }
+
+            let value = attr.unescape_and_decode_value(reader)?;
+            if let Ok(v) = value.parse::<f64>() {
+                match attr.key {
+                    b"lat" => {
+                        lat = v;
+                    }
+                    b"lon" => {
+                        lon = v;
+                    }
+                    _ => {
+                        panic!("shouldn't reach here");
+                    }
+                }
+            } else {
+                return Err(Error::ValueError(reader.buffer_position()));
+            }
+        }
+
+        if lat.is_nan() || lon.is_nan() {
+            return Err(Error::ValueError(reader.buffer_position()));
+        }
+
+        let (lat, lon) = convert_fn(lat, lon);
+        new_elem.push_attribute((LAT_NAME, lat.to_string().as_bytes()));
+        new_elem.push_attribute((LON_NAME, lon.to_string().as_bytes()));
+        Ok(new_elem)
+    })
+}
+
+fn main() -> Result<()> {
     let reader = quick_xml::Reader::from_file("demo/src.gpx").unwrap();
-    let writer =
-        quick_xml::Writer::new_with_indent(std::fs::File::create("demo/dst.gpx").unwrap(), b'\t', 1);
-    convert_gpx(reader, writer, undrift_gps::gcj_to_wgs).unwrap();
+    let writer = quick_xml::Writer::new_with_indent(
+        std::fs::File::create("demo/dst.gpx").unwrap(),
+        b'\t',
+        1,
+    );
+
+    gpx_transform(reader, writer, undrift_gps::gcj_to_wgs)
 }
